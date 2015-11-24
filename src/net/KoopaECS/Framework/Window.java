d1e03b8bddab155d06afed256c1102f43603886c
Aeomi <aeomei@gmail.com>
2015-11-23 20:57:04 -0500

    Bugs fixed, syntax changes

--- src/net/KoopaECS/Framework/Window.java
+++ src/net/KoopaECS/Framework/Window.java
@@ -2,15 +2,18 @@ package net.KoopaECS.Framework;
 
 import javax.swing.JFrame;
 
+
 public class Window extends JFrame {
 	
 	
 	
 	public Window(int x, int y, String title){
-		setSize(x,y);
+		
+		setSize(x, y);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle(title);
+		
 	}
 	
 }
